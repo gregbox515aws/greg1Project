60c7ade48d359d796d065714bf2fe19fcd40ba2e
Ubuntu <ubuntu@ip-172-31-25-239.us-west-2.compute.internal>
2013-07-26 03:05:57 +0000

    person_create set up

--- greg1App/views.py
+++ greg1App/views.py
@@ -1,12 +1,16 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response
+from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 
 from greg1App.models import Person
 
 def home(request):
 	return render_to_response('greg1App/home.html')
+
+class PersonCreateView(CreateView):
+	model = Person
 
 class PersonUpdateView(UpdateView):
 	model = Person
--- greg1Project/urls.py
+++ greg1Project/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import ListView, DetailView
 
 from greg1App.models import Person
-from greg1App.views import PersonUpdateView
+from greg1App.views import PersonCreateView, PersonUpdateView
 
 
 # Uncomment the next two lines to enable the admin:
@@ -25,6 +25,7 @@ urlpatterns = patterns('',
     url(r'^$', 'greg1App.views.home'),
     url(r'^person_list$', ListView.as_view(model=Person)),
     url(r'^person_detail/(?P<pk>\d+)$', DetailView.as_view(model=Person)),
+    url(r'^person_create$', PersonCreateView.as_view(success_url='/person_list')),
     url(r'^person_update/(?P<pk>\d+)$', PersonUpdateView.as_view(success_url='/person_list')),
     url(r'^person_delete/(?P<pk>\d+)$', DetailView.as_view(model=Person)),
 )
